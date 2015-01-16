ff5fc63af04bcd7c69db8de6a36d0681af08cb3c
pietdaniel <pietdaniel@gmail.com>
2015-01-16 09:05:16 -0500

    loader.s

--- pietos/loader.s
+++ pietos/loader.s
@@ -2,3 +2,13 @@ global loader
 
 MAGIC_NUMBER equ 0xDEADB33F
 CHECKSUM     equ -MAGIC_NUMBER 
+
+section .text:
+align 4
+  dd MAGIC_NUMBER
+  dd CHECKSUM
+
+loader:
+  mov eax, 0xDEADB337
+.loop:
+  jmp .loop
